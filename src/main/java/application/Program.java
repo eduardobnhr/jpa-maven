3117122e3c44de4276f85b6b81cd1c74e148c3a8
eduardo <eduardobnhr@gmail.com>
2023-07-27 11:10:35 -0400

    add insert and update in database

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,17 +1,28 @@
 package application;
 
+import javax.persistence.Embeddable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos Da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Alexandre Da Silva", "alexandre@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Isaias Da Silva", "isaias@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 		
 	}
 
